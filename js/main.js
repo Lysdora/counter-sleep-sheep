e1dfc4427ad6890ed573fb1eed6eff7074b6bfbe
Lysdora <lysdora.web@gmail.com>
2023-07-12 16:51:00 +0200

    fix img

--- js/main.js
+++ js/main.js
@@ -5,21 +5,6 @@ let sleepEl = document.getElementById("sleep");
 let resetEl = document.getElementById("reset");
 resetEl.addEventListener("click", reset);
 
-//Variable for control video
-let videoEl = document.getElementById("video");
-videoEl.playbackRate = 0.5;
-
-// Obtenez une référence à l'élément de bouton de lecture
-let playEl = document.getElementById("play");
-
-// Ajoutez un gestionnaire d'événements pour le clic sur le bouton de lecture
-playEl.addEventListener("click", function () {
-  // Vérifiez si la vidéo est en pause, puis démarrez la lecture
-  if (videoEl.paused) {
-    videoEl.play();
-  }
-});
-
 // A this is a "h2" element with a ID counter
 let counterEl = document.getElementById("counter");
 
